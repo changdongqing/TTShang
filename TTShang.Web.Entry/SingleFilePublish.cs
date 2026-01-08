bd0e5dded9c7abe8dca4e75ac09869b86a0e2f37
chdq0306 <chdq0306@qq.com>
2026-01-08 20:39:07 +0800

    furion初始化创建的

---
+++ TTShang.Web.Entry/SingleFilePublish.cs
@@ -0,0 +1,22 @@
+﻿using Furion;
+using System.Reflection;
+
+namespace TTShang.Web.Entry;
+
+public class SingleFilePublish : ISingleFilePublish
+{
+    public Assembly[] IncludeAssemblies()
+    {
+        return Array.Empty<Assembly>();
+    }
+
+    public string[] IncludeAssemblyNames()
+    {
+        return new[]
+        {
+            "TTShang.Application",
+            "TTShang.Core",
+            "TTShang.Web.Core"
+        };
+    }
+}

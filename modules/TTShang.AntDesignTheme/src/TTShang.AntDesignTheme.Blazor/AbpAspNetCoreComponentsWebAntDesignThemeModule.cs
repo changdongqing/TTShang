f80912a89caf1daa712fb6eb53120e91d2d03405
changdongqing <changdongqing@163.com>
2026-01-28 18:31:42 +0800

    antdesigntheme

---
+++ modules/TTShang.AntDesignTheme/src/TTShang.AntDesignTheme.Blazor/AbpAspNetCoreComponentsWebAntDesignThemeModule.cs
@@ -0,0 +1,21 @@
+﻿using TTShang.AntDesignUI;
+using Volo.Abp.AspNetCore.Components.Web.Security;
+using Volo.Abp.Modularity;
+using Volo.Abp.UI.Navigation;
+
+namespace TTShang.AntDesignTheme.Blazor;
+
+[DependsOn(
+    typeof(AbpAntDesignUIModule),
+    typeof(AbpUiNavigationModule)
+)]
+public class AbpAspNetCoreComponentsWebAntDesignThemeModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        Configure<AbpDynamicLayoutComponentOptions>(options =>
+        {
+            options.Components.Add(typeof(AbpAuthenticationState), null);
+        });
+    }
+}
